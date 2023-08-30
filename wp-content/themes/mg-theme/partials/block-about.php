b9e843823bc66a0045516e09c288ee61bc8ef4ee
Max Globa <onyx18121990@gmail.com>
2023-08-30 07:24:21 +0300

    Fix About us section

--- wp-content/themes/mg-theme/partials/block-about.php
+++ wp-content/themes/mg-theme/partials/block-about.php
@@ -1,21 +1,21 @@
 <section class="about">
   <div class="container">
-    <div class="about__info">
-      <h2><span>ABOUT</span> OUR COMPANY</h2>
-      <h3>Curabitur congue<br>egestas dapibus.</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non libero consectetur, blandit mauris eget, imperdiet nisl. Etiam commodo ex nec erat tempor varius. Nunc rutrum pretium nunc in malesuada. Curabitur mollis urna ac sapien vulputate, ut congue sapien vehicula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam et suscipit dui. Maecenas velit quam, mollis id erat eget, scelerisque elementum odio. Maecenas ac sagittis erat. Quisque hendrerit dapibus diam, a venenatis dui efficitur nec. Aenean tincidunt ullamcorper fermentum.</p>
-      <ul>
-        <li>Lorem ipsum dolor sit amet consectetur</li>
-        <li>Lorem ipsum dolor sit amet consectetur</li>
-        <li>Ut sit amet nisl sed ante vulputate</li>
-        <li>Ut sit amet nisl sed ante vulputate</li>
-        <li>Lorem ipsum dolor sit amet consectetur</li>
-        <li>Ut sit amet nisl sed ante vulputate</li>
-      </ul>
-      <button class="btn">contact us</button>
-    </div>
-    <div class="about__img">
+    <h2 class="section-title"><span>ABOUT</span> OUR COMPANY</h2>
+    <div class="about__row">
       <img src="<?php echo IMG; ?>/about-img.png" alt="">
+      <div class="about__info">
+        <h3>Curabitur congue<br>egestas dapibus.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non libero consectetur, blandit mauris eget, imperdiet nisl. Etiam commodo ex nec erat tempor varius. Nunc rutrum pretium nunc in malesuada. Curabitur mollis urna ac sapien vulputate, ut congue sapien vehicula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam et suscipit dui. Maecenas velit quam, mollis id erat eget, scelerisque elementum odio. Maecenas ac sagittis erat. Quisque hendrerit dapibus diam, a venenatis dui efficitur nec. Aenean tincidunt ullamcorper fermentum.</p>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur</li>
+          <li>Lorem ipsum dolor sit amet consectetur</li>
+          <li>Ut sit amet nisl sed ante vulputate</li>
+          <li>Ut sit amet nisl sed ante vulputate</li>
+          <li>Lorem ipsum dolor sit amet consectetur</li>
+          <li>Ut sit amet nisl sed ante vulputate</li>
+        </ul>
+        <button class="btn">contact us</button>
+      </div>
     </div>
   </div>
 </section>
