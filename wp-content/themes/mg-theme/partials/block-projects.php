5d36990e6333dddcfe2a9d203b4785b3db7b1d49
Max Globa <onyx18121990@gmail.com>
2023-08-31 09:07:17 +0300

    Fix projects

--- wp-content/themes/mg-theme/partials/block-projects.php
+++ wp-content/themes/mg-theme/partials/block-projects.php
@@ -2,9 +2,10 @@
   <div class="container">
     <h2 class="title"><span>LATEST</span> PROJECT</h2>
   </div>
-  <div class="projects__row-top">
-    <div class="projects__img"><img src="<?php echo IMG; ?>/projects/project1.png" alt="">
-      <div class="projects__info">
+  <div class="projects__row">
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project1.png" alt="">
+      <div>
         <h4>Home</h4>
         <ul>
           <li>Builder</li>
@@ -12,20 +13,115 @@
         </ul>
       </div>
     </div>
-    <div><img src="<?php echo IMG; ?>/projects/project2.png" alt=""></div>
-    <div><img src="<?php echo IMG; ?>/projects/project3.png" alt=""></div>
-    <div><img src="<?php echo IMG; ?>/projects/project4.png" alt=""></div>
-  </div>
-  <div class="projects__row-middle">
-    <div><img src="<?php echo IMG; ?>/projects/project5.png" alt=""></div>
-    <div><img src="<?php echo IMG; ?>/projects/project6.png" alt=""></div>
-    <div><img src="<?php echo IMG; ?>/projects/project7.png" alt=""></div>
-    <div><img src="<?php echo IMG; ?>/projects/project8.png" alt=""></div>
-  </div>
-  <div class="projects__row-bottom">
-    <div><img src="<?php echo IMG; ?>/projects/project9.png" alt=""></div>
-    <div><img src="<?php echo IMG; ?>/projects/project10.png" alt=""></div>
-    <div><img src="<?php echo IMG; ?>/projects/project11.png" alt=""></div>
-    <div><img src="<?php echo IMG; ?>/projects/project12.png" alt=""></div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project2.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project3.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project4.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project5.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project6.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project7.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project8.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project9.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project10.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project11.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
+    <div class="projects__item">
+      <img src="<?php echo IMG; ?>/projects/project12.png" alt="">
+      <div>
+        <h4>Home</h4>
+        <ul>
+          <li>Builder</li>
+          <li>Repairman</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </section>
