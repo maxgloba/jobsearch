e89567fe14edc24ac90a7c73ebb351b719dbb961
Max Globa <onyx18121990@gmail.com>
2023-09-27 07:30:24 +0300

    Fix jquery

--- wp-content/themes/mg-theme/functions.php
+++ wp-content/themes/mg-theme/functions.php
@@ -3,10 +3,10 @@
 define('ROOT', get_template_directory_uri());
 define('IMG', ROOT . '/img');
 
-// function replace_core_jquery_version() {
-// 	wp_deregister_script( 'jquery' );
-// 	wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.1.1.min.js", array(), '3.1.1' );
-// }
+function replace_core_jquery_version() {
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', "https://code.jquery.com/jquery-3.1.1.min.js", array(), '3.1.1' );
+}
 add_action( 'wp_enqueue_scripts', 'replace_core_jquery_version' );
 
 add_theme_support( 'post-thumbnails' );
